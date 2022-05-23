842bcfe63eb4f9a5fdc5ae872aae5f811f2b220b
zulrah <1s16slrse@mozmail.com>
2022-05-23 16:40:16 -0400

    Refactor code to move constants in its own module for code readability

--- src/main.rs
+++ src/main.rs
@@ -3,217 +3,16 @@
    Kraken Interview: Implement/Architect Order Book
 */
 
+//Modules
+mod helper;
+//Imports
+#[cfg(tests)]
+mod tests;
 use std::cell::{Cell, RefCell};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-//HELPER CONSTANTS
-static SKIP_LINE_CHAR: char = '#'; // Since CSV files do not allow comments we shall ignore lines that start with the pound/hashtag symbol.
-static CSV_COLUMN_SEPARATOR: char = ','; // TODO: Support the various seperators but comma is the most common.
-//EXPECTED CONSTANTS USED BY UNIT TESTS ONLY
-static EXPECTED_SCENARIO_1 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
 
-static EXPECTED_SCENARIO_2 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_3 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_4 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_5 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_6 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_7 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_8 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_9 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_10 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_11 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
-
-static EXPECTED_SCENARIO_12 : &str = 
-r#"
-A, 1, 1
-B, B, 10, 100
-A, 1, 2
-B, S, 12, 100
-A, 2, 101
-A, 2, 102
-B, S, 11, 100
-R, 1, 3
-R, 2, 103
-A, 1, 4
-B, B, 10, 200
-A, 2, 104
-B, S, 11, 200
-"#;
 
 #[derive(Clone, Copy)]
 enum TransactionStatus {
@@ -321,12 +120,12 @@ impl Order {
 
     // Parses the current CSV line will return None if current line is a comment, corrupted/invalid, or information is incomplete
     fn from(csv_line: &String) -> Option<Self> {
-        if csv_line.starts_with(SKIP_LINE_CHAR) {
+        if csv_line.starts_with(helper::constants::SKIP_LINE_CHAR) {
             None
         } else {
             //The columns where the data has not been transformed nor has been validated. We use map to convert the &str iterator to a String vector.
             let raw_column = csv_line
-                .split(CSV_COLUMN_SEPARATOR)
+                .split(helper::constants::CSV_COLUMN_SEPARATOR)
                 .map(|column| String::from(column))
                 .collect::<Vec<String>>();
             None //TODO: Implement logic
